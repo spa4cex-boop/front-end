69456c085fd2ff555b3deeb339e872ecd91c8022
spa4cex-boop <spa4cex@gmail.com>
2026-05-15 02:39:16 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    return config;
+  },
+};
+
+export default nextConfig;
